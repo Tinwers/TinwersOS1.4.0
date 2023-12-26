37b68e8ac71fd4c1dd3f6f429cdeafcd3c160f62
sunny blank <145170311+Tinwers@users.noreply.github.com>
2023-12-26 22:33:34 +0800

    Add files via upload

---
+++ Tinwersgame.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	system("title Tinwers Game [version:1.4.0]");
+	cout<<"Tinwers Game [version:1.4.0]"<<endl;
+	cout<<"(c) SunnyBlank 保留所有权利。"<<endl<<endl;
+	cout<<"请选择模式  1.贪吃蛇   2. 坦克  3.五子棋  [1/2/3] ";
+	int answer;
+	cin>>answer;
+	if(answer==1) system("start ys.exe");
+	if(answer==2) system("start ys2.exe");
+	if(answer==3) system("start ys3.exe");
+}
